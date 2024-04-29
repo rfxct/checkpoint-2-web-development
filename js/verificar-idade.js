67cd030bd946d6a815d2c0e2e0df2941ce582e4c
Marcos <xbrcvar@gmail.com>
2024-04-29 17:24:46 -0300

    adicionar script de verificação de idade

---
+++ js/verificar-idade.js
@@ -0,0 +1,9 @@
+function verificarIdade() {
+  const checked = localStorage.getItem("checked") === "true";
+  if (checked) return null;
+
+  const acimaDe18 = confirm("Você confirma que tem mais de 18 anos?");
+  if (acimaDe18) return localStorage.setItem("checked", true);
+
+  window.location = "/pages/errors/menor-idade.html";
+}
