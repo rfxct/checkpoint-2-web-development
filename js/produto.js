97b4ff4aa22d20d06c4230199959941844f4193a
Marcos <xbrcvar@gmail.com>
2024-04-29 18:03:47 -0300

    carregar imagem do produto

--- js/produto.js
+++ js/produto.js
@@ -1,10 +1,12 @@
 let precoDefault = 0,
   tituloDefault = '',
-  descricaoDefault = ''
+  descricaoDefault = '',
+  imagemDefault = ''
 
 const tituloElement = document.getElementById('product-title'),
   descricaoElement = document.getElementById('product-description'),
-  precoElement = document.getElementById('product-price')
+  precoElement = document.getElementById('product-price'),
+  imagemElement = document.getElementById('product-image')
 
 function carregarInformacoes() {
   const id = new URLSearchParams(location.search).get('id')
@@ -14,16 +16,19 @@ function carregarInformacoes() {
       precoDefault = 1243
       tituloDefault = "Vinho Tinto Tallarico"
       descricaoDefault = "Um vinho encorpado e elegante, com aromas de frutas vermelhas e notas de especiarias. Perfeito para acompanhar carnes vermelhas."
+      imagemDefault = 'tallarico.png'
       break;
     case 'gato-negro':
       precoDefault = 5199
       tituloDefault = "Vinho Tinto Gato Negro Carmenère "
       descricaoDefault = "Um vinho suave e equilibrado, com aromas de frutas negras e toques de baunilha. Ideal para harmonizar com massas e queijos."
+      imagemDefault = 'gato-negro.png'
       break;
     case 'dom-bosco':
       precoDefault = 1290
       tituloDefault = "Vinho Tinto Dom Bosco"
       descricaoDefault = "Um vinho intenso e complexo, com notas de frutas maduras e especiarias. Ótima escolha para acompanhar pratos de carne suína."
+      imagemDefault = 'dom-bosco.webp'
       break;
     default:
       precoDefault = 0
@@ -34,4 +39,6 @@ function carregarInformacoes() {
   tituloElement.innerText = tituloDefault
   descricaoElement.innerText = descricaoDefault
   precoElement.innerText = `R$ ${(precoDefault * .01).toLocaleString('pt-br')}`
+  imagemElement.src = `../images/${imagemDefault}`
+  imagemElement.alt = imagemDefault
 }
