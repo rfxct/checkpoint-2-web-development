ce1b4114cd057559bf5b2609ee760130a3ac2db4
Marcos <xbrcvar@gmail.com>
2024-04-29 22:28:49 -0300

    corrigir redirecionamento

--- js/verificar-idade.js
+++ js/verificar-idade.js
@@ -5,5 +5,5 @@ function verificarIdade() {
   const acimaDe18 = confirm("Você confirma que tem mais de 18 anos?");
   if (acimaDe18) return localStorage.setItem("checked", true);
 
-  window.location = `${location.href}/pages/errors/menor-idade.html`;
+  window.location = `${location.origin}/pages/errors/menor-idade.html`;
 }
